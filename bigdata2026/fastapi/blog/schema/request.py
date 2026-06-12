d547df0c2a891cc7526d202f0d811f3b190728d1
mykohbig7-ux <mykohbig7@gmail.com>
2026-06-12 16:03:59 +0900

    day25 메모장, 오늘 뭐머지 실습

--- bigdata2026/fastapi/blog/schema/request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class PostCreate(BaseModel):
-    title: str
-    content: str
-
-class PostUpdate(BaseModel):
-    title: str 
-    content: str 
--- bigdata2026/fastapi/blog/schema/response.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class PostResponse(BaseModel):
-    id: int
-    title: str
-    content: str
-
-class MessageResponse(BaseModel):
-    message: str
